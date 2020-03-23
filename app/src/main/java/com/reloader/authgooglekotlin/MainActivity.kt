6d98669cc34d9b4b83bf04f65d7b2449610523ed
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-03-23 12:00:59 -0500

    Initial commit

---
+++ app/src/main/java/com/reloader/authgooglekotlin/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.reloader.authgooglekotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+const val RC_SIGN_IN = 123
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            // .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        sign_in_button.visibility = View.VISIBLE
+        tv_name.visibility = View.GONE
+        sign_in_button.setSize(SignInButton.SIZE_STANDARD)
+        sign_in_button.setOnClickListener {
+
+            val signInIntent = mGoogleSignInClient.getSignInIntent()
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+            val acct = GoogleSignIn.getLastSignedInAccount(this)
+            if (acct != null) {
+                val personName = acct.displayName
+                val personGivenName = acct.givenName
+                val personFamilyName = acct.familyName
+                val personEmail = acct.email
+                val personId = acct.id
+                val personPhoto = acct.photoUrl
+
+                sign_in_button.visibility= View.GONE
+                tv_name.text = acct?.displayName
+                tv_name.visibility= View.VISIBLE
+                tv_token.text = acct?.idToken
+            }
+        }
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+
+            Log.v("name", account?.displayName)
+
+            sign_in_button.visibility= View.GONE
+            tv_name.text = account?.displayName
+            tv_name.visibility= View.VISIBLE
+            tv_token.text = account?.idToken
+
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("errorException", "signInResult:failed code=" + e.statusCode)
+            sign_in_button.visibility= View.VISIBLE
+            tv_name.text = ""
+            tv_name.visibility= View.GONE
+
+        }
+
+    }
+}
